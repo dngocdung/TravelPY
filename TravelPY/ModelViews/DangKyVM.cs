85c44b3d303fff1862cc3f35cd8e895022be0988
Dũng Đặng Ngọc <dangngocdung12001@gmail.com>
2023-02-09 19:11:57 +0700

    .

--- TravelPY/ModelViews/DangKyVM.cs
+++ TravelPY/ModelViews/DangKyVM.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.Xml.Linq;
 
-namespace Travel.ModelViews
+namespace TravelPY.ModelViews
 {
     public class DangKyVM
     {
--- TravelPY/ModelViews/DangNhapVM.cs
+++ TravelPY/ModelViews/DangNhapVM.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Xml.Linq;
 
-namespace Travel.ModelViews
+namespace TravelPY.ModelViews
 {
     public class DangNhapVM
     {
